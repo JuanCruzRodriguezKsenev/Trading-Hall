694654bcb351dd693f7d3c4525713ddfb6acc3ee
Rodriguez Juan Cruz <lacolinajc@gmail.com>
2026-01-15 20:26:01 -0300

    arreglo filter worlds page 3.0

--- src/components/ui/Card/index.tsx
+++ src/components/ui/Card/index.tsx
@@ -4,8 +4,18 @@ import styles from "./Card.module.css";
 interface CardProps {
   children: React.ReactNode;
   className?: string;
+  // Añadimos onClick a la interfaz para que TypeScript lo reconozca
+  onClick?: (e: React.MouseEvent<HTMLDivElement>) => void;
 }
 
-export const Card = ({ children, className }: CardProps) => {
-  return <div className={`${styles.card} ${className || ""}`}>{children}</div>;
+export const Card = ({ children, className, onClick }: CardProps) => {
+  return (
+    <div
+      className={`${styles.card} ${className || ""}`}
+      // Pasamos el evento al div real que se renderiza en el DOM
+      onClick={onClick}
+    >
+      {children}
+    </div>
+  );
 };
